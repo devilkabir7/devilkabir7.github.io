14690ef13f3c783e571e7b54c85e53c09e17132e
devilkabir7 <50204693+devilkabir7@users.noreply.github.com>
2022-04-25 22:34:10 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Personal Portfolio Website</title>
+		<link rel="stylesheet" href="Assignment_CSS.css">
+	</head>
+	<body>
+		<?php include "navigation.php"; ?>
+		<?php include "header.php"; ?>
+			<div class="content">
+				<span class="title">Engineering student</span>
+				<h1>Hello, I’m <span>Kabir, E19CSE104</span></h1>
+				<p>Currently pursuing my B.Tech in computer science engineering at Bennett University. Graduating in 2023.</p>
+			</div>
+		</div>
+		<?php include "footer.php"; ?>
+	</body>
+</html>
